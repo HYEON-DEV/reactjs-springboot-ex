c007c77bd1da5a07dc6be4491e9c6c3060603dac
SeunghyeonLee <hyeon970315@gmail.com>
2025-08-10 23:44:04 +0900

    서비스 개발 및 실습 - Retrieve Todo 구현

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -26,7 +26,7 @@ public class TodoController {
     @PostMapping
     public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
         try {
-            String temporaryUserId = "temporary-user";
+            String temporaryUserId = "temp-user";
 
             TodoEntity entity = TodoDTO.toEntity(dto);
             entity.setId(null);
@@ -50,6 +50,23 @@ public class TodoController {
         list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         // ResponseEntity.ok(response) 사용해도 됨
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temporaryUserId = "temp-user";
+
+        // (1) 서비스 메서드의 retrieve 메서드를 사용해 Todo 리스트를 가져온다.
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        // (2) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        // (3) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        // (4) ResponseDTO를 리턴한다
         return ResponseEntity.ok().body(response);
     }
 }
